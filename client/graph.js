bf467237b685026909afa30f1c184ef20ce5d470
metaphorically <metaphorically@gmail.com>
2009-11-13 18:26:08 -0500

    Changed from writing SVG directly to relying on the jsxgraph library. This allows goplot to focus on data processing not ... plotting.

---
+++ client/graph.js
@@ -0,0 +1,60 @@
+var graph1;
+ 
+function makeGraph(dataSeries) {
+  var i, x1, y1;
+  var p;
+  var points = [];
+  var x = [];
+  var y = [];
+  var start = 0;
+  var end = dataSeries.length;
+
+
+  var xmin=0, xmax=0, ymin=0, ymax=0; 
+  for (i=start;i<end;i++) {
+    // todo: there's a faster way to do this...
+    if (dataSeries[i].x < xmin) {
+      xmin = dataSeries[i].x;
+    } else if (dataSeries[i].x > xmax) {
+      xmax = dataSeries[i].x;
+    }
+    if (dataSeries[i].y < ymin) {
+      ymin = dataSeries[i].y;
+    } else if (dataSeries[i].y > ymax) {
+      ymax = dataSeries[i].y;
+    }
+  }
+
+  brd = JXG.JSXGraph.initBoard('jxgbox', {boundingbox: [xmin - 4, ymax + 4, xmax + 4, ymin - 4], axis: true});
+  brd.suspendUpdate();
+
+  //points.push(brd.createElement('point', [xmin,0], {visible:false, name:'', fixed:true}));
+  for (i=start;i<end;i++) {
+
+    x1 = dataSeries[i].x;
+    y1 = dataSeries[i].y;
+
+    // Plot it
+    p = brd.createElement('point', [x1,y1], 
+                  {strokeWidth:2, strokeColor:'#ffffff', 
+                   highlightStrokeColor:'#0077cc', fillColor:'#0077cc',  
+                   highlightFillColor:'#0077cc', style:6, name:'', fixed:true}
+                ); 
+    points.push(p);
+    x.push(x1);
+    y.push(y1);
+  }
+  // Filled area. We need two additional points [start,0] and [end,0]
+  //points.push(brd.createElement('point', [xmax,0], {visible:false, name:'', fixed:true}));
+  brd.createElement('polygon',points, {withLines:false,fillColor:'#e6f2fa'});
+ 
+  // Curve:
+  brd.createElement('curve', [x,y], 
+                 {strokeWidth:3, strokeColor:'#0077cc', 
+                  highlightStrokeColor:'#0077cc'}
+               );
+
+  brd.unsuspendUpdate();
+  
+  return brd;
+}
--- goplot.go
+++ goplot.go
@@ -8,22 +8,23 @@ import (
   "bytes";
   "strings";
   "strconv";
+  //"containers/vector";
 )
 
-// Plan:
-// - read a data file
-// - create path data
-// - write out the path in an svg file
+type Point struct {
+  x float;
+  y float;
+}
 
 func main() {
   
   const MAXLINES = 1000000;
   
   sourceFileName := flag.String("i","source.txt","Source data file name");
-  destFileName := flag.String("o","viz.svg","Output file name");
+  destFileName := flag.String("o","vizdata.js","Output file name");
 
-  fmt.Println(*sourceFileName);
-  fmt.Println(*destFileName);
+  //fmt.Println(*sourceFileName);
+  //fmt.Println(*destFileName);
 
    //read the data
    //f is a file.File*
@@ -36,45 +37,60 @@ func main() {
     fmt.Fprintf(os.Stderr, "failed to read %s: %s\n", *sourceFileName, errStr);
   }
   sourceBuf := bytes.NewBuffer(sourceData);
-  fmt.Println(sourceBuf.String());
+  //fmt.Println(sourceBuf.String());
   
   // split the buffer into an array of strings, one per source line
   src := sourceBuf.String();
   srcLines := strings.Split(src,"\n",MAXLINES);
 
   lineCount := len(srcLines);
-  plotPath := "";
-  if lineCount > 0 {
-    plotPath = "M"+srcLines[0];
-    for ix:=1 ; ix < lineCount ; ix++ {
-      x, y, err := parseLine(srcLines[ix]);
-      if err == nil {
-        plotPath += "L" + strconv.Ftoa(x,'f',6) + "," + strconv.Ftoa(y,'f',6) + " ";
-      }
+  // make a slice to hold the point data we read in
+  series := make([]Point, lineCount, lineCount);
+  
+  // if an error is returned we still save the value in the series array
+  //for ix:=0; ix < lineCount; ix++ {
+  //  series[ix] , err = parseLine(srcLines[ix]);
+  //}
+
+  // need to test for error before saving the value
+  stmp := Point{x:0.0, y:0.0};
+  for ix:=0; ix < lineCount; ix++ {
+    stmp , err = parseLine(srcLines[ix]);
+    if err == nil {
+      //fmt.Println(ix, stmp);
+      series[ix] = stmp;
     }
   }
-  svgStr := "<?xml version=\"1.0\"?>\n";
-  svgStr += "<svg xmlns:xlink=\"http://www.w3.org/1999/xlink\" xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 100 100\">\n";
-  svgStr += "<path stroke-width=\"2\" stroke=\"#BB5511\" fill=\"none\" d=\"" + plotPath + "\"/>\n";
-  svgStr += "</svg>\n";
 
-  fmt.Println(svgStr);
+
+  jsonStr:="series=[";
+  for ix:=0; ix < lineCount; ix++ {
+    // fmt.Printf("{x:%f,y:%f}",series[ix].x, series[ix].y);
+    jsonStr += "{x:" + strconv.Ftoa(series[ix].x,'f',3) + ",y:" + strconv.Ftoa(series[ix].y,'f',3) + "},";
+  }
+  jsonStr+="];";
+
+  //fmt.Println(jsonStr);
   
-  _=io.WriteFile(*destFileName, strings.Bytes(svgStr), 777);
+  _=io.WriteFile(*destFileName, strings.Bytes(jsonStr), 777);
 }
 
-func parseLine(coords string) (x float, y float, err os.Error) {
+func foo(p Point) {
+  
+}
+
+func parseLine(coords string) (p Point, err os.Error) {
   if len(coords) > 0 {
     coordsAr := strings.Split(strings.TrimSpace(coords), ",", 3);
     if len(coordsAr) > 1 {
       // ignore conversion errors
-      x, err = strconv.Atof(coordsAr[0]);
+      p.x, err = strconv.Atof(coordsAr[0]);
       if err == nil {
-        y, err = strconv.Atof(coordsAr[1]);
+        p.y, err = strconv.Atof(coordsAr[1]);
       }
     }
   } else {
     err = os.NewError("parseLine: No data");
   }
-  return x, y, err;
+  return p, err;
 }
